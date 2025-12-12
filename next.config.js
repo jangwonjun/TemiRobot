e62c3abe22cbb70fed2e7824e29aca2e15222558
jangwonjun <won2005won@naver.com>
2025-12-12 15:12:17 +0900

    fix: Next.js webpack 설정으로 temi-api-unified 청크 분리 방지
    - temi-api-unified.ts와 temi-webview-interface.ts를 메인 번들에 강제 포함
    - 청크 로드 404 에러 해결
    - 테미 로봇에서 통합 API 정상 작동하도록 수정

--- next.config.js
+++ next.config.js
@@ -8,6 +8,37 @@ const nextConfig = {
     TEMI_ROBOT_IP: process.env.TEMI_ROBOT_IP,
     TEMI_ROBOT_PORT: process.env.TEMI_ROBOT_PORT,
   },
+  webpack: (config, { isServer }) => {
+    // temi-api-unified를 메인 번들에 포함 (청크 분리 방지)
+    if (!isServer) {
+      config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+          ...config.optimization.splitChunks,
+          cacheGroups: {
+            ...config.optimization.splitChunks.cacheGroups,
+            // temi-api-unified 관련 파일을 메인 번들에 강제 포함
+            temiApi: {
+              test: /[\\/]src[\\/]lib[\\/]temi-api-unified\.(ts|tsx|js|jsx)$/,
+              name: false, // 메인 번들에 포함
+              chunks: 'all',
+              enforce: true,
+              priority: 30,
+            },
+            // temi-webview-interface도 메인 번들에 포함
+            temiWebView: {
+              test: /[\\/]src[\\/]lib[\\/]temi-webview-interface\.(ts|tsx|js|jsx)$/,
+              name: false, // 메인 번들에 포함
+              chunks: 'all',
+              enforce: true,
+              priority: 30,
+            },
+          },
+        },
+      }
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
